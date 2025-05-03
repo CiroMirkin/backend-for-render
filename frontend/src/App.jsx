b9c12712c20a6e8decc924ecb2f6225bb92426c5
CiroMirkin <ciromirkin@gmail.com>
2025-05-03 17:38:34 -0300

    avance: login de usuarios existentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,18 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Notelist from './components/NoteList'
 import noteService from './services/notes'
+import loginService from './services/login'
 import AddNoteForm from './components/AddNoteForm'
 import { deleteThisNote } from '../../domain/deleteNote'
 
 function App() {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
+
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  /** Save user information and authentication token */
+  const [ user, setUser ] = useState(null)
 
   useEffect(() => {
     noteService.getAllNotes().then((initialNotes) => {
@@ -35,10 +41,56 @@ function App() {
     })
   }
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } 
+    catch (exception) {
+      console.error('Wrong credentials')
+    }
+  }
+
+  const LoginForm = () => (
+    <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+  )
+
+  const deleteNoteFunction = user != null ? deleteNote : () => {}
+
   return (
     <>
       <h1>Notes</h1>
-      <Notelist notes={notes} deleteNote={deleteNote}/>
+      { user != null && <p>{user.name} logged-in</p> }
+      { user == null && LoginForm() }
+      
+      <Notelist notes={notes} deleteNote={ deleteNoteFunction }/>
       <AddNoteForm addNote={addNote} newNote={newNote} setNewNote={setNewNote} />
     </>
   )
